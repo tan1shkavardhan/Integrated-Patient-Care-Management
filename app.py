6a9b9933c0c4d51929a9bff9fe720ceec0c2757f
Tanishka Chouhan <chouhantanishka25@gmail.com>
2026-07-05 16:37:34 +0530

    Configure MySQL database and create user model

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
+from database.db import db
+from models.user import User
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "patientcare123"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:tan1shk%40@localhost/patientcare"
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+with app.app_context():
+    db.create_all()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ models/user.py
@@ -0,0 +1,11 @@
+from database.db import db
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    phone = db.Column(db.String(15), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
